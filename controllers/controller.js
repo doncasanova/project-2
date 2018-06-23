4f674aeaaa14ae5b8157b834f2a29642e853264d
doncasanova <ethanjuan@yahoo.com>
2018-06-23 12:31:53 -0500

    added server

---
+++ controllers/controller.js
@@ -0,0 +1,33 @@
+
+//--------------------------------------------------------------
+var express = require("express");
+console.log("contorller")
+// var router = express.Router();
+var db = require("../models");
+// create routes
+//---------------------------------
+// Sends to DOM
+// router.get("/", function (req, res) {
+//   burgers.selectAll(function (data) {
+//     res.render('index', { burgers: data });
+//   });
+// });
+
+// //Create a New Burger
+// router.post('/burger/create', function (req, res) {
+//   burgers.insertOne(req.body.burger, function () {
+//      res.redirect("/");
+//   });
+// });
+
+// //Devour a Burger
+// router.put('/burger/eat/:id', function (req, res) {
+//   burgers.updateOne(req.params.id, function () {
+//     res.render('index');
+//   });
+// });
+// 
+//---------------------------------------------------
+
+//Export routes
+// module.exports = router;

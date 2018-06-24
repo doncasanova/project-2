d47ee813ff74337c84c19d33b1822f56fd26ef6e
kyhakw <kyhakw@hotmail.com>
2018-06-24 15:45:03 -0500

    add change

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT into pref_type (name) VALUES ('sports');
+INSERT into  pref_type (name) VALUES ('arts');
--- server.js
+++ server.js
@@ -1,33 +1,40 @@
-
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 
+// Sets up the Express App
+// =============================================================
+var app = express();
 var PORT = process.env.PORT || 8080;
 
-var app = express();
+// Requiring our models for syncing
+var db = require("./models");
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// app.use(express.static("view"));
+// Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Static directory
+app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Routes
+// =============================================================
+//require("./controllers/html-routes.js")(app);
+require("./controllers/user-api-controller.js")(app);
+//require("./routes/post-api-routes.js")(app);
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/controller.js");
-
-// app.use(routes);
-
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });

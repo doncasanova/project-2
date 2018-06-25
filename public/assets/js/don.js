a46747c440bb579de3c5459ac80326fe20e5bb38
doncasanova <ethanjuan@yahoo.com>
2018-06-24 22:47:03 -0500

    more examples

--- public/assets/js/don.js
+++ public/assets/js/don.js
@@ -1,37 +1,43 @@
 
 var ticketType = ['hockey', 'football', 'baseball', 'basketball', 'soccer', 'concerts', 'events of all kinds', 'exhibits']
+mainPage()
+function mainPage() {
+    $(document).ready(function () {
+        $(".navBarRollDown").slideUp();
+        $(".userPreference").on("click", function (event) {
+            // Make sure to preventDefault on a submit event.
+            event.preventDefault();
+            console.log('clicked on preference')
+        });
 
+        $(".userLogIn").on("click", function (event) {
+            // Make sure to preventDefault on a submit event.
+            event.preventDefault();
 
-$(document).ready(function () {
+            $(".navBarRollDown").slideDown();
+            $(".slideUpLogIn").slideUp();
+            loadAnimatedLeftSideColunm(ticketType)
+            window.setTimeout(function () {
+                loadAnimatedMiddleSideColunm(ticketType)
+            }, 500);
+            window.setTimeout(function () {
+                loadAnimatedRightSideColunm(ticketType)
+            }, 1000);
 
-    $(".userPreference").on("click", function (event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
-        console.log('clicked on preference')
+            $(".LogOutClick").on("click", function (event) {
+                event.preventDefault();
+                mainPage()
+                
+            });
+        });
     });
-
-    $(".userLogIn").on("click", function (event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
-        loadAnimatedLeftSideColunm(ticketType)
-    window.setTimeout(function () {
-        loadAnimatedMiddleSideColunm(ticketType)
-    }, 500);
-    window.setTimeout(function () {
-        loadAnimatedRightSideColunm(ticketType)
-    }, 1000);
-   
-    });
-
-    
-});
-
+}
 
 function loadAnimatedLeftSideColunm(ticketType) {
 
     for (var i = 0; i < ticketType.length; i++) {
         console.log(ticketType[1])
-        var div = $('<h6 class = "waterFallHeader" >Buy,Sell,Trade</h6><p>' + ticketType[i] + '</p>').prependTo('.waterFallSlideLeft');
+        var div = $('<h6 class = "waterFallHeader p-1" >Buy,Sell,Trade</h6><p>' + ticketType[i] + '</p>').prependTo('.waterFallSlideLeft');
 
         var height = div.outerHeight();
         div.css({
@@ -48,7 +54,7 @@ function loadAnimatedMiddleSideColunm(ticketType) {
 
     for (var i = 0; i < ticketType.length; i++) {
         console.log(ticketType[1])
-        var div = $('<h6 class = "waterFallHeader" >Buy,Sell,Trade</h6><p>' + ticketType[i] + '</p>').prependTo('.waterFallSlideMiddle');
+        var div = $('<h6 class = "waterFallHeader p-1" >Buy,Sell,Trade</h6><p>' + ticketType[i] + '</p>').prependTo('.waterFallSlideMiddle');
 
         var height = div.outerHeight();
         div.css({
@@ -65,7 +71,7 @@ function loadAnimatedRightSideColunm(ticketType) {
 
     for (var i = 0; i < ticketType.length; i++) {
         console.log(ticketType[1])
-        var div = $('<h6 class = "waterFallHeader" >Buy,Sell,Trade</h6><p>' + ticketType[i] + '</p>').prependTo('.waterFallSlideRight');
+        var div = $('<h6 class = "waterFallHeader p-1" >Buy,Sell,Trade</h6><p>' + ticketType[i] + '</p>').prependTo('.waterFallSlideRight');
 
         var height = div.outerHeight();
         div.css({

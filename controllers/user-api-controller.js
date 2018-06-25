a6d76577dca9b8d1abb90b1c8bb4daa34cdf2891
doncasanova <ethanjuan@yahoo.com>
2018-06-24 19:27:14 -0500

    added waterfall div for demo

--- controllers/user-api-controller.js
+++ controllers/user-api-controller.js
@@ -17,11 +17,7 @@ module.exports = function(app) {
   
     res.render("don");
   });
-  // test added by don casanova
-  app.get("/don/landing", function(req, res) {
-  
-    res.render("donlandingpage");
-  });
+
 
 }
 
--- public/assets/js/don.js
+++ public/assets/js/don.js
@@ -1,3 +1,7 @@
+
+var ticketType = ['hockey', 'football', 'baseball', 'basketball', 'soccer', 'concerts', 'events of all kinds', 'exhibits']
+
+
 $(document).ready(function () {
 
     $(".userPreference").on("click", function (event) {
@@ -9,29 +13,67 @@ $(document).ready(function () {
     $(".userLogIn").on("click", function (event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
-        console.log('user log in click')
+        loadAnimatedLeftSideColunm(ticketType)
+    window.setTimeout(function () {
+        loadAnimatedMiddleSideColunm(ticketType)
+    }, 500);
+    window.setTimeout(function () {
+        loadAnimatedRightSideColunm(ticketType)
+    }, 1000);
+   
     });
 
-    loadAnimatedText()
+    
 });
 
 
-function loadAnimatedText() {
-    // $('#gen').click(function () {
-var test = 5
-        for (var i = 0; i < test; i++) {
-            var div = $('<h6>Another</h6><p>Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>').prependTo('.rainbowSlide');
+function loadAnimatedLeftSideColunm(ticketType) {
 
-            var height = div.outerHeight();
-            div.css({
-                "marginTop": -height,
-                "opacity": 0
-            }).animate({
-                "marginTop": 0,
-                "opacity": 1
-            }, "slow");
-        };
-    // });
+    for (var i = 0; i < ticketType.length; i++) {
+        console.log(ticketType[1])
+        var div = $('<h6 class = "waterFallHeader" >Buy,Sell,Trade</h6><p>' + ticketType[i] + '</p>').prependTo('.waterFallSlideLeft');
 
+        var height = div.outerHeight();
+        div.css({
+            "marginTop": -height,
+            "opacity": 0
+        }).animate({
+            "marginTop": 0,
+            "opacity": 1
+        }, "slow");
+    };
+};
 
+function loadAnimatedMiddleSideColunm(ticketType) {
+
+    for (var i = 0; i < ticketType.length; i++) {
+        console.log(ticketType[1])
+        var div = $('<h6 class = "waterFallHeader" >Buy,Sell,Trade</h6><p>' + ticketType[i] + '</p>').prependTo('.waterFallSlideMiddle');
+
+        var height = div.outerHeight();
+        div.css({
+            "marginTop": -height,
+            "opacity": 0
+        }).animate({
+            "marginTop": 0,
+            "opacity": 1
+        }, "slow");
+    };
+};
+
+function loadAnimatedRightSideColunm(ticketType) {
+
+    for (var i = 0; i < ticketType.length; i++) {
+        console.log(ticketType[1])
+        var div = $('<h6 class = "waterFallHeader" >Buy,Sell,Trade</h6><p>' + ticketType[i] + '</p>').prependTo('.waterFallSlideRight');
+
+        var height = div.outerHeight();
+        div.css({
+            "marginTop": -height,
+            "opacity": 0
+        }).animate({
+            "marginTop": 0,
+            "opacity": 1
+        }, "slow");
+    };
 };

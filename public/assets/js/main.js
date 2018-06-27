b73fe32c4689af2a79b2bb32f5b576ad554d5ebf
doncasanova <ethanjuan@yahoo.com>
2018-06-26 19:23:22 -0500

    reconfig files

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,1 @@
 
-
-		$(document).ready(function() {
- 
-            $("#testimonial-container").owlCarousel({
-           
-                navigation : false, // Show next and prev buttons
-                slideSpeed : 700,
-                paginationSpeed : 400,
-                singleItem:true,
-            });
-   
-          });
